2e1dd269128c392df1039833ae5e2bb881be938f
Sean O <27134812+SeanoNET@users.noreply.github.com>
2019-08-19 20:48:19 +0800

    moved mqtt client out of program into mqtt service

---
+++ RoomTempMQTTConsumer/RoomTempMQTTConsumer/MqttService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.IO;
+
+namespace RoomTempMQTTConsumer
+{
+    internal class MqttService
+    {
+        private readonly DataRepository _dataRepository;
+        private IMqttClient _mqttClient;
+        private IMqttClientOptions _mqttClientOptions;
+
+        public MqttService(IConfiguration configuration)
+        {
+            _dataRepository = new DataRepository(configuration.GetSection("DataSource").Value);
+
+            var mqqtConfig = configuration.GetSection("MqttClient");   
+            
+            int port = 1883;
+            Int32.TryParse(mqqtConfig.GetSection("MqttServerPort").Value, out port);
+
+            // MQTT options
+            _mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithClientId(mqqtConfig.GetSection("ClientId").Value)
+                .WithTcpServer(mqqtConfig.GetSection("MqttServerIp").Value, port)
+                .WithCleanSession()
+                .Build();
+
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+        }
+        public void Run()
+        {
+            Console.WriteLine("Running");
+        }
+    }
+}
--- RoomTempMQTTConsumer/RoomTempMQTTConsumer/Program.cs
+++ RoomTempMQTTConsumer/RoomTempMQTTConsumer/Program.cs
@@ -9,9 +9,8 @@ namespace RoomTempMQTTConsumer
 {
     class Program
     {
-        private static DataRepository _dataRepository;
-        private static IMqttClient _mqttClient;
-        private static IMqttClientOptions _mqttClientOptions;
+        
+        private static MqttService _mqttService;
 
         static void ConfigureService()
         {
@@ -21,29 +20,19 @@ namespace RoomTempMQTTConsumer
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
 
-            var mqqtConfig = config.GetSection("MqttClient");
-
-            int port = 1883;
-            Int32.TryParse(config.GetSection("MqttServerPort").Value, out port);
-
-            // MQTT options
-            _mqttClientOptions = new MqttClientOptionsBuilder()
-                .WithClientId(config.GetSection("ClientId").Value)
-                .WithTcpServer(config.GetSection("MqttServerIp").Value, port)
-                .WithCleanSession()
-                .Build();
-
-            var factory = new MqttFactory();
-            _mqttClient = factory.CreateMqttClient();
-
-
-            _dataRepository = new DataRepository(config.GetSection("DataSource").Value);
+            _mqttService = new MqttService(config);                 
         }
+
         static void Main(string[] args)
         {
             ConfigureService();
 
-            Console.WriteLine("Running");
+
+            _mqttService.Run();
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
     }
 }
